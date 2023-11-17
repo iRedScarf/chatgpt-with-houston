00995516333a7eabbd05cdc982ed82458277cb13
eks <kimzer@vista.aero>
2023-11-17 15:41:35 +0800

    feat: use Markdown-It

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://chatgpt-with-houston.vercel.app/', // process.env.DEPLOY_URL || 
+  site: 'https://chatgpt-with-houston.vercel.app/',
   output: "server",
-  adapter: vercel()
+  adapter: vercel({ analytics: true }),
 });
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import MarkdownIt from 'markdown-it';
+
 export async function makeAskRequest(message: string): Promise<any> {
   try {
     const response = await fetch('/api', {
@@ -19,24 +21,8 @@ export async function makeAskRequest(message: string): Promise<any> {
   }
 }
 
+const md = new MarkdownIt();
+
 export function markdown(input: string): string {
-  return input
-    .replace(
-      /(```|~~~)(\w*)([\s\S]+?)\1/g,
-      (_1, _2, lang: string, raw: string) => {
-        const code = raw
-          .replace(/</g, "&lt;")
-          .replace(/>/g, "&gt;")
-          .replace(/`/g, "&#96;");
-        const classes = lang ? ` class="language-${lang}"` : "";
-        return `<pre><code${classes}>${code.trim()}</code></pre>`;
-      }
-    )
-    .replace(/`([^`\n]*)`/g, (_1, raw) => {
-      return raw
-        ? `<code>${raw.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</code>`
-        : "";
-    })
-    .replace(/\*\*(.*?)\*\*/g, (_2, raw) => `<b>${raw}</b>`)
-    .replace(/\*(.*?)\*/g, (_3, raw) => `<em>${raw}</em>`);
+  return md.render(input);
 }
