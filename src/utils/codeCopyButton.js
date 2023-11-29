ebec8f380ce8266fba91c7623b23994f7303197b
eks <kimzer@vista.aero>
2023-11-29 21:43:16 +0800

    feat: add copied & copy failed

--- src/utils/codeCopyButton.js
+++ src/utils/codeCopyButton.js
@@ -28,25 +28,27 @@ export function addCopyButton(bubble) {
     copyButton.style.top = codeBlock.offsetTop + 10 + "px";
     copyButton.style.right = "10px";
     copyButton.onclick = function () {
-      navigator.clipboard.writeText(codeBlock.innerText).then(() => {
-        copyButton.innerHTML = `
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Copied</title>
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"
-              fill="currentColor"
-            ></path>
-          </svg>`;
-        setTimeout(() => {
+      navigator.clipboard
+        .writeText(codeBlock.innerText)
+        .then(() => {
           copyButton.innerHTML = `
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Copied</title>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"
+                fill="currentColor"
+              ></path>
+            </svg>`;
+          setTimeout(() => {
+            copyButton.innerHTML = `
             <svg
               width="16"
               height="16"
@@ -68,8 +70,49 @@ export function addCopyButton(bubble) {
                 fill="currentColor"
               ></path>
             </svg>`;
-        }, 1250);
-      });
+          }, 1250);
+        })
+        .catch(() => {
+          copyButton.innerHTML = `
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Copy failed</title>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm9.78-2.22-5.5 5.5a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734l5.5-5.5a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042Z"
+              fill="currentColor"></path>
+          </svg>`;
+          setTimeout(() => {
+            copyButton.innerHTML = `
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Copy</title>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"
+                fill="currentColor"
+              ></path>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"
+                fill="currentColor"
+              ></path>
+            </svg>`;
+          }, 1500);
+        });
     };
     codeBlock.parentNode.style.position = "relative";
     codeBlock.parentNode.appendChild(copyButton);
